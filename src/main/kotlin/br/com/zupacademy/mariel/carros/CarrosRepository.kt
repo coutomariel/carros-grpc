c05c42a0573e0686113c4565cd85818640fd6a7a
Mariel Couto <coutomariel@gmail.com>
2021-07-14 13:58:11 -0300

    Modelagem e configuração do banco de dados

---
+++ src/main/kotlin/br/com/zupacademy/mariel/carros/CarrosRepository.kt
@@ -0,0 +1,8 @@
+package br.com.zupacademy.mariel.carros
+
+import io.micronaut.data.annotation.Repository
+import io.micronaut.data.jpa.repository.JpaRepository
+
+@Repository
+interface CarrosRepository: JpaRepository<Carro, Long> {
+}
